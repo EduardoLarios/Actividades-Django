f2d30c5372b7e06f2709b543818f0e2eec91ee3b
Esteban Quintana <a01184855@itesm.mx>
2017-01-22 22:57:39 -0600

    Setting up django - Virtual enviroment setup

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/esteban/myenv"
+set -gx VIRTUAL_ENV "/home/esteban/Documentos/Repositorios/Actividades-Django/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- myenv/bin/django-admin.py
+++ myenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/esteban/myenv/bin/python
+#!/home/esteban/Documentos/Repositorios/Actividades-Django/myenv/bin/python
 # EASY-INSTALL-SCRIPT: 'Django==1.10.5','django-admin.py'
 __requires__ = 'Django==1.10.5'
 __import__('pkg_resources').run_script('Django==1.10.5', 'django-admin.py')
